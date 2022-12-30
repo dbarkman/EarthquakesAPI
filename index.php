2bc5d6205864a286920dbba958e66b1eecf9639b
David Barkman <david.barkman13@gmail.com>
2022-12-29 23:48:35 -0700

    Expanded API to support RapidAPI.com. Expanded API further to support the new Every Earthquake iOS app.

--- index.php
+++ index.php
@@ -16,34 +16,41 @@ $earthquakesAPI->validateAPICommon();
 
 if (isset($_REQUEST['noun'])) {
     if ($_REQUEST['noun'] === 'testPass') {
-        $earthquakesAPI->testPass();
-
-    } else if ($_REQUEST['noun'] === 'earthquakes') {
         if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-            $earthquakesAPI->validateGetEarthquakes();
-            $parameters = array();
-            if (isset($_REQUEST['recent']) && $_REQUEST['recent'] == true) {
-                $parameters['interval'] = $_REQUEST['interval'];
-            } else if (isset($_REQUEST['timePeriod']) && $_REQUEST['timePeriod'] == true) {
-                $parameters['startDate'] = $_REQUEST['startDate'];
-                $parameters['endDate'] = $_REQUEST['endDate'];
+            $earthquakesAPI->testPass();
+        } else {
+            $earthquakesAPI->badRequest();
+        }
+
+    } else if ($_REQUEST['noun'] === 'token') {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            if (isset($_REQUEST['token']) && isset($_REQUEST['debug'])) {
+                $earthquakesAPI->createToken($_REQUEST['token'], $_REQUEST['debug']);
             } else {
-                $maxCount = 1000;
-                if (isset($_REQUEST['count'])) {
-                    if ($_REQUEST['count'] > $maxCount) {
-                        $parameters['count'] = $maxCount;
-                    } else {
-                        $parameters['count'] = $_REQUEST['count'];
-                    }
-                } else {
-                    $parameters['count'] = $maxCount;
-                }
+                $earthquakesAPI->badRequest('You must include both \'token\' and \'debug\' variables when calling the \'token\' endpoint with POST.');
             }
-            if (isset($_REQUEST['location']) && $_REQUEST['location'] == true) {
-                $parameters['latitude'] = $_REQUEST['latitude'];
-                $parameters['longitude'] = $_REQUEST['longitude'];
-                $parameters['radius'] = $_REQUEST['radius'];
-                $parameters['units'] = $_REQUEST['units'];
+        } else {
+            $earthquakesAPI->badRequest('There was another problem with your post.');
+        }
+
+    } else if (strpos(strtolower($_REQUEST['noun']), 'earthquakes') !== false) {
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+            $parameters = array();
+            $maxCount = 1000;
+            $maxLocationCount = 100;
+            $maxStart = 100000;
+            $parameters['count'] = 100;
+//            $parameters['start'] = 0;
+            if (isset($_REQUEST['count']) && $_REQUEST['count'] <= $maxCount) {
+                $parameters['count'] = $_REQUEST['count'];
+            }
+            if (isset($_REQUEST['start'])) {
+                if ($_REQUEST['start'] <= $maxStart) {
+                    $parameters['start'] = $_REQUEST['start'];
+                } else {
+                    $earthquakesAPI->badRequest('Using parameter \'start\' with a value higher than 100k is not allowed. Use the \'earthquakesByDate\' endpoint with \'startDate\', \'endDate\', \'count\' and \'start\' parameters to narrow down your search and reduce the number of pages you need to retrieve.');
+                }
             }
             if (isset($_REQUEST['magnitude'])) {
                 $parameters['magnitude'] = $_REQUEST['magnitude'];
@@ -54,10 +61,152 @@ if (isset($_REQUEST['noun'])) {
             if (isset($_REQUEST['type'])) {
                 $parameters['type'] = $_REQUEST['type'];
             }
-            if (isset($_REQUEST['hotSpot'])) {
-                $parameters['hotSpot'] = $_REQUEST['hotSpot'];
+            if (isset($_REQUEST['order'])) {
+                $parameters['order'] = $_REQUEST['order'];
+            }
+
+            $locationData = 0;
+            if (isset($_REQUEST['latitude'])) {
+                $parameters['latitude'] = $_REQUEST['latitude'];
+                $locationData++;
+            }
+            if (isset($_REQUEST['longitude'])) {
+                $parameters['longitude'] = $_REQUEST['longitude'];
+                $locationData++;
+            }
+            if (isset($_REQUEST['radius'])) {
+                $parameters['radius'] = $_REQUEST['radius'];
+                $locationData++;
+            }
+            if (isset($_REQUEST['units'])) {
+                $parameters['units'] = $_REQUEST['units'];
+                $locationData++;
+            }
+            $parameters['location'] = false;
+            if ($locationData == 4) {
+                $parameters['location'] = true;
+                if ($parameters['count'] > $maxLocationCount) {
+                    $earthquakesAPI->badRequest('When searching based on a location, using parameter \'count\' with a value higher than 100 is not allowed. Use the paging parameters \'count\' and \'start\' together, to retrieve the data you need.');
+                }
+            } else if ($locationData != 0) {
+                $earthquakesAPI->badRequest('Parameter \'latitude\', \'longitude\', \'radius\' or \'units\' is missing. All four are required when searching by location.');
+            }
+
+            if ($_REQUEST['noun'] == 'recentEarthquakes') {
+                if (isset($_REQUEST['interval'])) {
+                    $parameters['interval'] = $_REQUEST['interval'];
+                } else {
+                    $earthquakesAPI->badRequest('Parameter \'interval\' required when calling \'recentEarthquakes\'. See ISO8601 Duration documentation for interval format.');
+                }
+            } else if ($_REQUEST['noun'] == 'earthquakesByDate') {
+                if (isset($_REQUEST['startDate']) && isset($_REQUEST['endDate'])) {
+                    $parameters['startDate'] = $_REQUEST['startDate'];
+                    $parameters['endDate'] = $_REQUEST['endDate'];
+                } else {
+                    $earthquakesAPI->badRequest('Parameter \'startDate\' and \'endDate\' are required when calling \'earthquakesByDate\'. Use YYYY-MM-DD format.');
+                }
             }
             $earthquakesAPI->getEarthquakes($parameters);
+        } else {
+            $earthquakesAPI->badRequest();
+        }
+
+    } else if ($_REQUEST['noun'] === 'latestEarthquakeNearMe') {
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+            if (isset($_REQUEST['latitude']) && isset($_REQUEST['longitude'])) {
+                $parameters['latest'] = true;
+                $parameters['latitude'] = $_REQUEST['latitude'];
+                $parameters['longitude'] = $_REQUEST['longitude'];
+                $earthquakesAPI->getEarthquakes($parameters);
+            } else {
+                $earthquakesAPI->badRequest('Parameters \'latitude\' and \'longitude\' required when calling \'latestEarthquakeNearMe\'.');
+            }
+        } else {
+            $earthquakesAPI->badRequest();
+        }
+
+    } else if (strpos(strtolower($_REQUEST['noun']), 'significant') !== false) {
+        $parameters['count'] = -1;
+        $parameters['significance'] = 600;
+        if (strpos(strtolower($_REQUEST['noun']), 'hour') !== false) {
+            $parameters['interval'] = 'PT1H';
+        } else if (strpos(strtolower($_REQUEST['noun']), 'day') !== false) {
+            $parameters['interval'] = 'PT24H';
+        } else if (strpos(strtolower($_REQUEST['noun']), 'week') !== false) {
+            $parameters['interval'] = 'P7D';
+        } else if (strpos(strtolower($_REQUEST['noun']), 'month') !== false) {
+            $parameters['interval'] = 'P30D';
+        } else {
+            $earthquakesAPI->resourceNotFound();
+        }
+        $earthquakesAPI->getEarthquakes($parameters);
+
+    } else if (strpos(strtolower($_REQUEST['noun']), '4.5') !== false) {
+        $parameters['count'] = -1;
+        $parameters['magnitude'] = 4.5;
+        if (strpos(strtolower($_REQUEST['noun']), 'hour') !== false) {
+            $parameters['interval'] = 'PT1H';
+        } else if (strpos(strtolower($_REQUEST['noun']), 'day') !== false) {
+            $parameters['interval'] = 'PT24H';
+        } else if (strpos(strtolower($_REQUEST['noun']), 'week') !== false) {
+            $parameters['interval'] = 'P7D';
+        } else if (strpos(strtolower($_REQUEST['noun']), 'month') !== false) {
+            $parameters['interval'] = 'P30D';
+        } else {
+            $earthquakesAPI->resourceNotFound();
+        }
+        $earthquakesAPI->getEarthquakes($parameters);
+
+    } else if (strpos(strtolower($_REQUEST['noun']), '2.5') !== false) {
+        $parameters['count'] = -1;
+        $parameters['magnitude'] = 2.5;
+        if (strpos(strtolower($_REQUEST['noun']), 'hour') !== false) {
+            $parameters['interval'] = 'PT1H';
+        } else if (strpos(strtolower($_REQUEST['noun']), 'day') !== false) {
+            $parameters['interval'] = 'PT24H';
+        } else if (strpos(strtolower($_REQUEST['noun']), 'week') !== false) {
+            $parameters['interval'] = 'P7D';
+        } else if (strpos(strtolower($_REQUEST['noun']), 'month') !== false) {
+            $parameters['interval'] = 'P30D';
+        } else {
+            $earthquakesAPI->resourceNotFound();
+        }
+        $earthquakesAPI->getEarthquakes($parameters);
+
+    } else if (strpos(strtolower($_REQUEST['noun']), '1.0') !== false) {
+        $parameters['count'] = -1;
+        $parameters['magnitude'] = 1.0;
+        if (strpos(strtolower($_REQUEST['noun']), 'hour') !== false) {
+            $parameters['interval'] = 'PT1H';
+        } else if (strpos(strtolower($_REQUEST['noun']), 'day') !== false) {
+            $parameters['interval'] = 'PT24H';
+        } else if (strpos(strtolower($_REQUEST['noun']), 'week') !== false) {
+            $parameters['interval'] = 'P7D';
+        } else if (strpos(strtolower($_REQUEST['noun']), 'month') !== false) {
+            $parameters['interval'] = 'P30D';
+        } else {
+            $earthquakesAPI->resourceNotFound();
+        }
+        $earthquakesAPI->getEarthquakes($parameters);
+
+    } else if (strpos(strtolower($_REQUEST['noun']), 'all') !== false) {
+        $parameters['count'] = -1;
+        if (strpos(strtolower($_REQUEST['noun']), 'hour') !== false) {
+            $parameters['interval'] = 'PT1H';
+        } else if (strpos(strtolower($_REQUEST['noun']), 'day') !== false) {
+            $parameters['interval'] = 'PT24H';
+        } else if (strpos(strtolower($_REQUEST['noun']), 'week') !== false) {
+            $parameters['interval'] = 'P7D';
+        } else if (strpos(strtolower($_REQUEST['noun']), 'month') !== false) {
+            $parameters['interval'] = 'P30D';
+        } else {
+            $earthquakesAPI->resourceNotFound();
+        }
+        $earthquakesAPI->getEarthquakes($parameters);
+
+    } else if ($_REQUEST['noun'] === 'types') {
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+            $earthquakesAPI->getTypes();
         } else {
             $earthquakesAPI->badRequest();
         }
@@ -187,13 +336,14 @@ class EarthquakesAPI
         $this->completeRequest();
     }
 
-    public function badRequest()
+    public function badRequest($error = '')
 	{
 		http_response_code(400);
 		$errorCode = 'badRequest';
-		$friendlyError = 'Bad Request';
-		$errors = array($friendlyError);
-		$this->echoResponse($errorCode, $errors, $friendlyError, 'fail', (object)array());
+		$errorString = 'Bad Request';
+        $friendlyError = (!empty($error)) ? $error : $errorString;
+		$errors = array($errorString);
+		$this->echoResponse($errorCode, $errors, $friendlyError, 'fail', array());
 		$this->completeRequest();
 	}
 
@@ -203,7 +353,7 @@ class EarthquakesAPI
 		$errorCode = 'resourceNotFound';
 		$friendlyError = 'Resource Not Found';
 		$errors = array($friendlyError);
-		$this->echoResponse($errorCode, $errors, $friendlyError, 'fail', (object)array());
+		$this->echoResponse($errorCode, $errors, $friendlyError, 'fail', array());
 		$this->completeRequest();
 	}
 
@@ -213,7 +363,7 @@ class EarthquakesAPI
 		$errorCode = 'resourceNotDefined';
 		$friendlyError = 'Resource Not Defined';
 		$errors = array($friendlyError);
-		$this->echoResponse($errorCode, $errors, $friendlyError, 'fail', (object)array());
+		$this->echoResponse($errorCode, $errors, $friendlyError, 'fail', array());
 		$this->completeRequest();
 	}
 
@@ -261,18 +411,77 @@ class EarthquakesAPI
 		$this->completeRequest();
 	}
 
-	///////////////////////////////////////////////////////////////////////////////
-	////////////////////////////// EARTHQUAKE FUNCTIONS ///////////////////////////
-	///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////// HELPER FUNCTIONS ///////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
 
-	public function getEarthquakes($parameters)
-	{
-		http_response_code(200);
-		$earthquakes = Earthquakes::GetEarthquakes($this->_db, $this->_logger, $parameters);
-		$response = $earthquakes;
-		$this->echoResponse('none', array(), '', 'success', $response);
-		$this->completeRequest();
-	}
+    public function string_contains (string $haystack, string $needle) {
+        return empty($needle) || strpos($haystack, $needle) !== false;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////// EARTHQUAKE FUNCTIONS ///////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+
+    public function getEarthquakes($parameters)
+    {
+        http_response_code(200);
+        $response = Earthquakes::GetEarthquakes($this->_db, $this->_logger, $parameters);
+        $this->echoResponse('none', array(), '', 'success', $response);
+        $this->completeRequest();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////// Types FUNCTIONS ////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+
+    public function getTypes()
+    {
+        http_response_code(200);
+        $response = Types::GetTypes($this->_logger, $this->_db);
+        $this->echoResponse('none', array(), '', 'success', $response);
+        $this->completeRequest();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////// TOKEN FUNCTIONS ////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+
+    public function createToken($token, $debug)
+    {
+        $sendPush = $_POST['sendPush'] ?? 0;
+        $magnitude = $_POST['magnitude'] ?? 6;
+        $location = $_POST['location'] ?? 0;
+        $radius = $_POST['radius'] ?? 0;
+        $units = $_POST['units'] ?? '';
+        $latitude = $_POST['latitude'] ?? 0;
+        $longitude = $_POST['longitude'] ?? 0;
+        $tokenObject = new Token($this->_logger, $this->_db, $token, $debug, $sendPush, $magnitude, $location, $radius, $units, $latitude, $longitude);
+        if ($tokenObject->getTokenExists() === 0) {
+            if ($tokenObject->saveToken() === FALSE) {
+                http_response_code(500);
+                $errorCode = 'tokenNotInserted';
+                $friendlyError = 'Token could not be inserted.';
+                $errors = array($friendlyError);
+                $this->echoResponse($errorCode, $errors, $friendlyError, 'fail', array());
+            } else {
+                http_response_code(201);
+                $this->echoResponse('none', array(), '', 'success', array());
+            }
+        } else {
+            if ($tokenObject->updateToken() === FALSE) {
+                http_response_code(500);
+                $errorCode = 'tokenNotUpdated';
+                $friendlyError = 'Token could not be updated.';
+                $errors = array($friendlyError);
+                $this->echoResponse($errorCode, $errors, $friendlyError, 'fail', array());
+            } else {
+                http_response_code(200);
+                $this->echoResponse('none', array(), '', 'success', array());
+            }
+        }
+        $this->completeRequest();
+    }
 
     ///////////////////////////////////////////////////////////////////////////////
 	////////////////////////////// CLOSING FUNCTIONS //////////////////////////////
@@ -322,6 +531,7 @@ class EarthquakesAPI
 		$this->logIt('info', 'Payload Size: ' . $this->_size);
         $this->logIt('info', 'Count: ' . $this->_count);
 		$this->logIt('info', 'Memory Usage: ' . $this->_memoryUsage);
+		$this->logIt('info', 'HTTP Response: ' . http_response_code());
 		$this->logIt('info', 'API Session Ended');
 		$this->logIt('info', '--------------------------------------------------------------------------------');
 		$this->logIt('info', '');
